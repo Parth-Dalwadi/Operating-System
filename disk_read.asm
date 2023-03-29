9417cd91162fc4713e52bc27c6c5bcdfd9e24771
Parth Dalwadi <dalwadip77@gmail.com>
2023-03-29 10:20:06 -0400

    Added basic keyboard input to OS.

--- disk_read.asm
+++ disk_read.asm
@@ -1,7 +1,7 @@
 disk_test:
 	pusha
 	mov ah, 0x02
-	mov al, 0x09
+	mov al, 0x1B
 	mov bx, KERNEL_ADDRESS
 	mov ch, 0x00
 	mov cl, 0x02
@@ -9,7 +9,7 @@ disk_test:
 	mov dl, [BOOT_DRIVE]
 	int 0x13
 	jc error_message
-	cmp al, 0x09
+	cmp al, 0x1B
 	jne error_message	
 	popa
 	ret	
--- kernel.c
+++ kernel.c
@@ -1,10 +1,17 @@
 #include "console.h"
+#include "keyboard.h"
 void main(){
 	clear_terminal();
 	print_character('H');
 	print_character('e');
 	print_line("llo World");
 	print_string("TODAY");
+	unsigned char byte;
+	while (1) {
+		while (byte = scan()) {
+			print_character(charmap[byte]);
+		}
+	}
 	//print_character_with_color("A", BLACK, YELLOW);
 	//print_character_with_color("A", 0x00, 0x0E);
 
