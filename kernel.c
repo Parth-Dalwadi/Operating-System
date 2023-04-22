30e6fe1bb0d901ef8bf9790484794c6b00990b04
Parth Dalwadi <dalwadip77@gmail.com>
2023-04-21 23:04:14 -0400

    Removed unnecessary comments in kernel.

--- kernel.c
+++ kernel.c
@@ -1,9 +1,5 @@
 #include "console.h"
 #include "keyboard.h"
-//char terminal_command[64];
-//char terminal_options[128];
-//int count = 0;
-//int space = 0;
 
 void main(){
 	clear_terminal();
@@ -11,12 +7,6 @@ void main(){
 	update_cursor();
 	unsigned char byte;
 	while (1) {
-		//if (read(byte) == 1){
-		//	print_integer(2);
-		//	break;
-		//} else{
-		//	read(byte);
-		//}
 		int exit = read(byte);
 		if (exit == 1){
 			break;
